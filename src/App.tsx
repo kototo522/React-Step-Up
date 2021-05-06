8e6a0d5ca93033600787bd4b86610653b6a31aff
kototo522 <huyejiben@gmail.com>
2021-05-07 01:30:03 +0900

    add: ヘッダーの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>Hello World</p>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
