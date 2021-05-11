b9f6aa421fbf6a8922aa904b618e87ffddec0796
kototo522 <huyejiben@gmail.com>
2021-05-11 16:02:28 +0900

    add: ユーザー詳細モーダル画面の作成

--- src/components/pages/UsersManagement.tsx
+++ src/components/pages/UsersManagement.tsx
@@ -1,17 +1,31 @@
-import { VFC, memo } from "react";
-import { WrapItem, Wrap } from "@chakra-ui/react";
+import { VFC, memo, useEffect } from "react";
+import { WrapItem, Wrap, Spinner, Center } from "@chakra-ui/react";
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hocks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+    const { getUsers, users, loading } = useAllUsers()
+
+    useEffect(() => getUsers(), [])
     return(
-        <Wrap p={{ base: 4, md: 10 }}>
-            <WrapItem>
-                <UserCard
-                    imageUrl="https://source.unsplash.com/random"
-                    userName="ことりん"
-                    fullName="yosimoto kotoha"
-                />
-            </WrapItem>
-        </Wrap>
+        <>
+        {loading ? (
+            <Center h="100vh">
+                <Spinner/>
+            </Center>
+        ) : (
+            <Wrap p={{ base: 4, md: 10 }}>
+                    {users.map((user) => (
+                        <WrapItem key={user.id} mg="auto">
+                            <UserCard
+                        imageUrl="https://source.unsplash.com/random"
+                        userName={user.username}
+                        fullName={user.name}
+                            />
+                        </WrapItem>
+                    ))}
+            </Wrap>
+        ) }
+        </>
     );
 });
