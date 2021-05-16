87d2fbdafbd64c9ec9c2b7e10bf29c5e30eba6c0
kototo522 <huyejiben@gmail.com>
2021-05-16 11:38:35 +0900

    add: 管理者ユーザーを想定してフラグをContextに保持する

--- src/components/pages/UsersManagement.tsx
+++ src/components/pages/UsersManagement.tsx
@@ -10,8 +10,8 @@ export const UserManagement: VFC = memo(() => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const { getUsers, users, loading } = useAllUsers()
     const { onSelectUser, selectedUser } = useSelectUser();
-    const { LoginUser } = useLoginUser();
-    console.log(LoginUser);
+    const { loginUser } = useLoginUser();
+    console.log(loginUser);
 
     useEffect(() => getUsers(), [])
 
--- src/providers/LoginUserProviders.tsx
+++ src/providers/LoginUserProviders.tsx
@@ -2,9 +2,11 @@ import { useState, createContext, Dispatch, ReactNode, SetStateAction } from "re
 
 import { User } from "../types/api/User";
 
+type LoginUser = User & { isAdmin: boolean }
+
 export type LoginUserContextType = {
-    LoginUser: User | null
-    setLoginUser: Dispatch<SetStateAction<User | null>>
+    loginUser: LoginUser | null
+    setLoginUser: Dispatch<SetStateAction<LoginUser | null>>
 }
 
 export const LoginUserContext = createContext<LoginUserContextType>(
@@ -13,9 +15,9 @@ export const LoginUserContext = createContext<LoginUserContextType>(
 
 export const LoginUserProviders = (props: { children: ReactNode }) => {
     const { children } = props;
-    const [LoginUser, setLoginUser] = useState<User | null>(null);
+    const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
     return (
-        <LoginUserContext.Provider value={{ LoginUser, setLoginUser }}>
+        <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
             {children}
         </ LoginUserContext.Provider>
     )
